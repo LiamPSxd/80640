b4aca6e655fd6e99dac4d4403a27b05407827499
LiamPSxd <zS20018158@estudiantes.uv.mx>
2022-11-03 14:48:38 -0600

    Script

--- practica06/src/main/java/mx/uv/c80640/App.java
+++ practica06/src/main/java/mx/uv/c80640/App.java
@@ -1,5 +1,8 @@
 package mx.uv.c80640;
 
+import static spark.Spark.*;
+import com.google.gson.JsonObject;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,20 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        get("/hello", (req, res) -> "Hello World");
+        get("/eduardo", (req, res) -> "Hello Eduardo");
+        get("/alex", (req, res) -> "Hello Alex");
+        get("/brandon", (req, res) -> "Hello Brandon");
+        get("/", (req, res) -> "<h1>Bienvenido</h1><img src='.jpg'>");
+
+        post("/", (req, res) ->{
+            System.out.println(req.queryParams("email")+" "+ req.queryParams("password"));
+            System.out.println(req.body());
+            res.status(200);
+            JsonObject oRespuesta= new JsonObject();
+            oRespuesta.addProperty("msj","hola");
+            oRespuesta.addProperty("email", req.queryParams("email"));
+            return oRespuesta;
+        });
     }
 }
